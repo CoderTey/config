3c0e8e2ba5cb949cb2e0a03753cef8cbf3eb063d
CoderTey <skoptey1@gmail.com>
2025-10-17 20:37:29 +0200

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,10 @@
-{ config, pkgs, lib, ... }:
+
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 {
   imports = [
@@ -18,40 +24,59 @@
   time.timeZone = "Europe/Berlin";
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
-    LC_ADDRESS        = "de_DE.UTF-8";
+    LC_ADDRESS = "de_DE.UTF-8";
     LC_IDENTIFICATION = "de_DE.UTF-8";
-    LC_MEASUREMENT    = "de_DE.UTF-8";
-    LC_MONETARY       = "de_DE.UTF-8";
-    LC_NAME           = "de_DE.UTF-8";
-    LC_NUMERIC        = "de_DE.UTF-8";
-    LC_PAPER          = "de_DE.UTF-8";
-    LC_TELEPHONE      = "de_DE.UTF-8";
-    LC_TIME           = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
   };
 
-  # ====== Xserver & Desktop ======
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.windowManager.qtile.enable = true;
+
+services.displayManager.ly.enable = true;
+  services.xserver = {
+    enable = true;
+    autoRepeatDelay = 200;
+    autoRepeatInterval = 35;
+    windowManager.oxwm.enable = true;
+    desktopManager.gnome.enable = true;
+    displayManager.sessionCommands = ''
+      xwallpaper --zoom ~/nixos-dotfiles/walls/wall1.png
+    '';
+  };
+
+
+services.picom = {
+  enable = true;
+  backend = "glx";
+  fade = true;
+};
+
+ programs.niri.enable = true;
   services.xserver.xkb = {
-    layout  = "us";
+    layout = "us";
     variant = "";
   };
 
   # ====== Users ======
   users.users.tey = {
-    isNormalUser  = true;
-    description   = "Tey";
-    extraGroups   = [ "networkmanager" "wheel" ];
-    shell         = pkgs.fish;
-    packages      = with pkgs; [ ];
+    isNormalUser = true;
+    description = "Tey";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    shell = pkgs.zsh;
+    packages = with pkgs; [ ];
   };
 
   # ====== Programs ======
   programs.nix-ld.enable = true;
   programs.firefox.enable = true;
-  programs.fish.enable = true;
+  programs.zsh.enable = true;
 
   # ====== Services ======
   services.printing.enable = true;
@@ -65,96 +90,142 @@
 
   # ====== Nix settings ======
   nixpkgs.config.allowUnfree = true;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
   # ====== Environment packages ======
-  environment.systemPackages = with pkgs; [
-    coreutils
-    gnumake
-    gcc
-    binutils
-    git
-    htop
-    ncdu
-    wget
-    curl
-    unzip
-    tree
-    pkgs.home-manager
-    xorg.xorgserver
-    xorg.xinit
-    xorg.xrandr
-    dwl
-    flatpak
-    hyprland
-    hyprlock
-    hyprpaper
-    waybar
-    sxhkd
-    rofi
-    firefox
-    zoom-us
-    spotify
-    discord
-    obsidian
-    libreoffice
-    telegram-desktop
-    nwg-look
-    dracula-icon-theme
-    xfce.thunar
-    pavucontrol
-    alacritty
-    foot
-    wmenu
-    tmux
-    zellij
-    fish
-    neovim
-    helix
-    vim
-    clang
-    clang-tools
-    cmake
-    rust-analyzer
-    rustup
-    clippy
-    rustfmt
-    rustc
-    zig
-    gh
-    nodejs_24
-    python314
-    lua-language-server
-    lua54Packages.luarocks-nix
-    cargo
-    fzf
-    ripgrep
-    zoxide
-    neofetch
-    fastfetch
-    lf
-    picom
-    scrot
-    wl-clipboard
-    lazygit
-    st
-    dmenu
-    alsa-tools
-    alsa-utils
-    tree-sitter
-    tree-sitter-grammars.tree-sitter-nix
-    fishPlugins.done
-    fishPlugins.fzf-fish
-    fishPlugins.forgit
-    fishPlugins.hydro
-    fishPlugins.grc
-    grc
-    nixd
-    ghostty
-    tokyo-night-gtk
-  ] ++ builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
+  environment.systemPackages =
+    with pkgs;
+    [
+      coreutils
+      gnumake
+      gcc
+      binutils
+      wlroots
+      zig
+      nordic
+      wayland-protocols
+      libxkbcommon
+      emacs
+      emacsPackages.doom
+      libevdev
+      pixman
+      pamixer
+      pkg-config
+      swaybg
+      grim
+      scdoc
+      mesa
+      egl-wayland
+      git
+      zsh-autocomplete
+      zsh-autopair
+      zsh-autosuggestions
+      htop
+      ncdu
+      wget
+      curl
+      unzip
+      tree
+      scenefx
+      sway
+      dwl
+      wlroots
+      pkgs.home-manager
+      xorg.xorgserver
+      xorg.xinit
+      xorg.xrandr
+      dwl
+      flatpak
+      hyprland
+      hyprlock
+      hyprpaper
+      waybar
+      sxhkd
+      rofi
+      firefox
+      zoom-us
+      spotify
+      discord
+      obsidian
+      libreoffice
+      telegram-desktop
+      nwg-look
+      dracula-icon-theme
+      xfce.thunar
+      pavucontrol
+      alacritty
+      foot
+      wmenu
+      tmux
+      zellij
+      fish
+      neovim
+      helix
+      vim
+      clang
+      clang-tools
+      cmake
+      rust-analyzer
+      rustup
+      clippy
+      rustfmt
+      rustc
+      zig
+      pkg-config
+      xorg.libX11
+      xorg.libXft
+      xorg.libXrender
+      freetype
+      fontconfig
+      nodejs_24
+      python314
+      lua-language-server
+      lua54Packages.luarocks-nix
+      cargo
+      fzf
+      ripgrep
+      zoxide
+      gnome
+      pcmanfm
+      nitch   
+      xwallpaper
+      lshw
+      fuzzel
+      vimPlugins.harpoon
+      vimPlugins.nord-nvim
+      vimPlugins.telescope-nvim
+      vimPlugins.lualine-nvim
+      vimPlugins.tokyonight-nvim
+      libnotify
+      pkgs.source-code-pro
+      neofetch
+      fastfetch
+      lf
+      picom
+      scrot
+      wl-clipboard
+      lazygit
+      st
+      dmenu
+      alsa-tools
+      alsa-utils
+      tree-sitter
+      tree-sitter-grammars.tree-sitter-nix
+      fishPlugins.done
+      fishPlugins.fzf-fish
+      fishPlugins.forgit
+      fishPlugins.hydro
+      fishPlugins.grc
+      grc
+      nixd
+      ghostty
+      tokyo-night-gtk
+    ]
+    ++ builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
 
   # ====== System version ======
   system.stateVersion = "25.05";
 }
-
--- nvidia.nix
+++ nvidia.nix
@@ -1,41 +1,39 @@
 { config, lib, pkgs, ... }:
-
 {
-  hardware.graphics = {
-    enable = true;
-  };
+    hardware.graphics = {
+        enable = true;
+    };
 
-  services.xserver.videoDrivers = ["nvidia"];
+    services.xserver.videoDrivers = ["nvidia" "modesetting"];
 
-  hardware.nvidia = {
-    modesetting.enable = true;
+    hardware.nvidia = {
+        modesetting.enable = true;
 
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-    # of just the bare essentials.
-    powerManagement.enable = false;
+        # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+        # Enable this if you have graphical corruption issues or application crashes after waking
+        # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+        # of just the bare essentials.
+        powerManagement.enable = false;
 
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
+        # Fine-grained power management. Turns off GPU when not in use.
+        # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+        powerManagement.finegrained = false;
 
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    open = false;
+        # Use the NVidia open source kernel module (not to be confused with the
+        # independent third-party "nouveau" open source driver).
+        open = false;
 
-    # Enable the Nvidia settings menu,
-	  # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
+        # Enable the Nvidia settings menu,
+        # accessible via `nvidia-settings`.
+        nvidiaSettings = true;
 
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+        # Optionally, you may need to select the appropriate driver version for your specific GPU.
+        package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
 
     hardware.nvidia.prime = {
-      sync.enable = true;
-		  intelBusId = "PCI:0:2:0";
-		  nvidiaBusId = "PCI:1:0:0";
-	  };
-  };
+        sync.enable = true;
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+    };
 }
-
